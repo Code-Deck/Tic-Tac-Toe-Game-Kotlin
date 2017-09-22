a3e9667c7449da6cc4dd0a8535618944b86fcaf9
CodeDeckClub <32191189+CodeDeckClub@users.noreply.github.com>
2017-09-22 01:38:08 -0700

    Update TicTacToe.kt

--- TicTacToe/src/TicTacToe.kt
+++ TicTacToe/src/TicTacToe.kt
@@ -28,7 +28,7 @@ fun printGame() {
 }
 
 fun claimPosition(playerName: String, row1: Array<String>, row2: Array<String>, row3: Array<String>) {
-    println("Here is the up to date Tic Tac Toe map!")
+    println("\nHere is the up to date Tic Tac Toe map!")
     printGame()
 
     println()
@@ -36,31 +36,31 @@ fun claimPosition(playerName: String, row1: Array<String>, row2: Array<String>, 
     println("Please select a row (1, 2, or 3)")
     val selectedRow: Int = readLine()!!.toInt()
 
-    print("Please select a position (1, 2, or 3)")
+    println("Please select a position (1, 2, or 3)")
     val selectedPosition: Int = readLine()!!.toInt()
 
     when (selectedRow) {
         1 ->
-            if (row1[selectedPosition - 1] == "Unclaimed") {
-                row1[selectedPosition - 1] = playerName
-            }
-            else if (row1[selectedPosition - 1] != "Unclaimed") {
-                println("Sorry. The position you have selected is already claimed. It is now your opponent's turn to play.")
-            }
+                if (row1[selectedPosition - 1] == "Unclaimed") {
+                    row1[selectedPosition - 1] = playerName
+                }
+                else if (row1[selectedPosition - 1] != "Unclaimed") {
+                    println("Sorry. The position you have selected is already claimed. It is now your opponent's turn to play.")
+                }
         2 ->
-            if (row2[selectedPosition - 1] == "Unclaimed") {
-                row2[selectedPosition - 1] = playerName
-            }
-            else if (row2[selectedPosition - 1] != "Unclaimed") {
-                println("Sorry. The position you have selected is already claimed. It is now your opponent's turn to play.")
-            }
+                if (row2[selectedPosition - 1] == "Unclaimed") {
+                    row2[selectedPosition - 1] = playerName
+                }
+                else if (row2[selectedPosition - 1] != "Unclaimed") {
+                    println("Sorry. The position you have selected is already claimed. It is now your opponent's turn to play.")
+                }
         3 ->
-            if (row3[selectedPosition - 1] == "Unclaimed") {
+                if (row3[selectedPosition - 1] == "Unclaimed") {
                 row3[selectedPosition - 1] = playerName
-            }
-            else if (row3[selectedPosition - 1] != "Unclaimed") {
-                println("Sorry. The position you have selected is already claimed. It is now your opponent's turn to play.")
-            }
+                }
+                else if (row3[selectedPosition - 1] != "Unclaimed") {
+                    println("Sorry. The position you have selected is already claimed. It is now your opponent's turn to play.")
+                }
     }
 
 }
@@ -80,98 +80,117 @@ fun main(Args: Array<String>) {
     while (row1.contains("Unclaimed") || row2.contains("Unclaimed") || row3.contains("Unclaimed")) {
         claimPosition(playerName, row1, row2, row3)
         if (row1.contains("Unclaimed") == false && row2.contains("Unclaimed") == false && row3.contains("Unclaimed") == false) {
-            println("Game Over.")
+            println("\nGame Over.\n")
+            printGame()
             break
         }
 
         if (row1[0] == "X" && row1[1] == "X" && row1[2] == "X") {
-            println("Game Over. Player X is the winner.")
+            println("\nGame Over. Player X is the winner.\n")
+            printGame()
             break
         }
         if (row2[0] == "X" && row2[1] == "X" && row2[2] == "X") {
-            println("Game Over. Player X is the winner.")
+            println("\nGame Over. Player X is the winner.\n")
+            printGame()
             break
         }
         if (row3[0] == "X" && row3[1] == "X" && row3[2] == "X") {
-            println("Game Over. Player X is the winner.")
+            println("\nGame Over. Player X is the winner.\n")
+            printGame()
             break
         }
         if (row1[0] == "X" && row2[1] == "X" && row3[2] == "X") {
-            println("Game Over. Player X is the winner.")
+            println("\nGame Over. Player X is the winner.\n")
             break
         }
         if (row3[0] == "X" && row2[1] == "X" && row1[2] == "X") {
-            println("Game Over. Player X is the winner.")
+            println("\nGame Over. Player X is the winner.\n")
+            printGame()
             break
         }
 
         if (row1[0] == "O" && row1[1] == "O" && row1[2] == "O") {
-            println("Game Over. Player O is the winner.")
+            println("\nGame Over. Player O is the winner.\n")
+            printGame()
             break
         }
         if (row2[0] == "O" && row2[1] == "O" && row2[2] == "O") {
-            println("Game Over. Player O is the winner.")
+            println("\nGame Over. Player O is the winner.\n")
+            printGame()
             break
         }
         if (row3[0] == "O" && row3[1] == "O" && row3[2] == "O") {
-            println("Game Over. Player O is the winner.")
+            println("\nGame Over. Player O is the winner.\n")
+            printGame()
             break
         }
         if (row1[0] == "O" && row2[1] == "O" && row3[2] == "O") {
-            println("Game Over. Player O is the winner.")
+            println("\nGame Over. Player O is the winner.\n")
+            printGame()
             break
         }
         if (row3[0] == "O" && row2[1] == "O" && row1[2] == "O") {
-            println("Game Over. Player O is the winner.")
+            println("\nGame Over. Player O is the winner.\n")
+            printGame()
             break
         }
 
         claimPosition(opponentName, row1, row2, row3)
 
         if (row1.contains("Unclaimed") == false && row2.contains("Unclaimed") == false && row3.contains("Unclaimed") == false) {
-            println("Game Over.")
+            println("\nGame Over.\n")
+            printGame()
             break
         }
 
         if (row1[0] == "X" && row1[1] == "X" && row1[2] == "X") {
-            println("Game Over. Player X is the winner.")
+            println("\nGame Over. Player X is the winner.\n")
+            printGame()
             break
         }
         if (row2[0] == "X" && row2[1] == "X" && row2[2] == "X") {
-            println("Game Over. Player X is the winner.")
+            println("\nGame Over. Player X is the winner.\n")
+            printGame()
             break
         }
         if (row3[0] == "X" && row3[1] == "X" && row3[2] == "X") {
-            println("Game Over. Player X is the winner.")
+            println("\nGame Over. Player X is the winner.\n")
+            printGame()
             break
         }
         if (row1[0] == "X" && row2[1] == "X" && row3[2] == "X") {
-            println("Game Over. Player X is the winner.")
+            println("\nGame Over. Player X is the winner.\n")
+            printGame()
             break
         }
         if (row3[0] == "X" && row2[1] == "X" && row1[2] == "X") {
-            println("Game Over. Player X is the winner.")
+            println("\nGame Over. Player X is the winner.\n")
             break
         }
 
         if (row1[0] == "O" && row1[1] == "O" && row1[2] == "O") {
-            println("Game Over. Player O is the winner.")
+            println("\nGame Over. Player O is the winner.\n")
+            printGame()
             break
         }
         if (row2[0] == "O" && row2[1] == "O" && row2[2] == "O") {
-            println("Game Over. Player O is the winner.")
+            println("\nGame Over. Player O is the winner.\n")
             break
         }
         if (row3[0] == "O" && row3[1] == "O" && row3[2] == "O") {
-            println("Game Over. Player O is the winner.")
+            println("\nGame Over. Player O is the winner.\n")
+            printGame()
             break
         }
         if (row1[0] == "O" && row2[1] == "O" && row3[2] == "O") {
-            println("Game Over. Player O is the winner.")
+            println("\nGame Over. Player O is the winner.\n")
+            printGame()
             break
         }
         if (row3[0] == "O" && row2[1] == "O" && row1[2] == "O") {
-            println("Game Over. Player O is the winner.")
+            println("\nGame Over. Player O is the winner.\n")
+            printGame()
             break
         }
     }
